5dd8467c8c267611923410749c2cfc14ac471b53
dev-owdenmag <mag.owden@gmail.com>
2025-02-12 07:11:42 +0000

    Beta Test #1

--- frontend/public/src/App.jsx
+++ frontend/public/src/App.jsx
@@ -1,25 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
 import AdminDashboard from "./pages/AdminDashboard";
 import ClientDashboard from "./pages/ClientDashboard";
-import Login from "./pages/Login";
-import RSVP from "./pages/RSVP";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-function App() {
+const App = () => {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/client" element={<ClientDashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/rsvp" element={<RSVP />} />
       </Routes>
       <Footer />
-    </Router>
+    </>
   );
-}
+};
 
 export default App;
--- frontend/public/src/main.jsx
+++ frontend/public/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./styles/index.css"; // Ensure you have a global CSS file
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
